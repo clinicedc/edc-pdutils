e1b3edb22725805f21b0cd4efe5595085ca24ffe
erikvw <ew2789@gmail.com>
2017-08-27 08:45:24 +0200

    refactor for inlines

--- edc_pdutils/df_preppers/__init__.py
+++ edc_pdutils/df_preppers/__init__.py
@@ -1,4 +1,3 @@
 from .crf_df_prepper import CrfDfPrepper
-from .crf_inline_df_prepper import CrfInlineDfPrepper
 from .df_prepper import DfPrepper
 from .non_crf_df_prepper import NonCrfDfPrepper
--- edc_pdutils/df_preppers/crf_inline_df_prepper.py
+++
@@ -1,6 +0,0 @@
-from .non_crf_df_prepper import NonCrfDfPrepper
-
-
-class CrfInlineDfPrepper(NonCrfDfPrepper):
-
-    pass
