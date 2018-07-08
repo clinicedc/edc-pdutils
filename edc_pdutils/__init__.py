8e422d31c6c584ddea47137d673d831eabb77704
erikvw <ew2789@gmail.com>
2018-07-08 20:40:36 +0200

    minor

--- edc_pdutils/__init__.py
+++ edc_pdutils/__init__.py
@@ -8,6 +8,7 @@ from .database import Database
 from .df_handlers import CrfDfHandler, NonCrfDfHandler, DfHandler
 from .dialects import CrfDialect, MysqlDialect, RsDialect
 from .model_to_dataframe import ModelToDataframe, SubjectModelToDataframe
+from .model_to_dataframe import ValueGetter
 from .table_to_dataframe import TableToDataframe
 from .tables import Aliquot, Consent, Visit, Requisition
 from .utils import datetime_to_date
