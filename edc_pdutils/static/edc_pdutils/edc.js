e29e54b615aa17383dce78f7fc2d8dfece18597b
erikvw <ew2789@gmail.com>
2018-09-21 14:21:32 -0500

    move styles and scripts to files

---
+++ edc_pdutils/static/edc_pdutils/edc.js
@@ -0,0 +1,11 @@
+$(window).bind("load", function () {
+    $('#work-in-progress').hide();
+});
+
+function selectAll(app_config_name,label){
+    $( ":input[id^=chk_" + app_config_name + "_" + label + "]" ).prop('checked', $("#select_all_" + app_config_name + "_" + label).prop('checked'));
+};
+
+function toggle(id){
+    $("input[id=" + id +"]").prop('checked', !$("input[id=" + id +"]").prop('checked'));
+}
