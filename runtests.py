3dc580f1fc07454624f557bed1cd1f797be2917c
erikvw <ew2789@gmail.com>
2019-04-10 00:06:15 -0500

    tox

--- runtests.py
+++ runtests.py
@@ -1,110 +1,49 @@
 #!/usr/bin/env python
-import os
 import django
 import logging
+import os
 import sys
 
 from django.conf import settings
 from django.test.runner import DiscoverRunner
-from os.path import abspath, dirname, join
+from edc_test_utils import DefaultTestSettings
+from os.path import abspath, dirname
 
 
-class DisableMigrations:
-
-    def __contains__(self, item):
-        return True
-
-    def __getitem__(self, item):
-        return None
-
-
-base_dir = dirname(abspath(__file__))
 app_name = 'edc_pdutils'
+base_dir = dirname(abspath(__file__))
 
-installed_apps = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "django.contrib.sites",
-    "django_extensions",
-    "django_crypto_fields.apps.AppConfig",
-    "django_revision.apps.AppConfig",
-    "edc_appointment.apps.AppConfig",
-    "edc_metadata.apps.AppConfig",
-    "edc_visit_tracking.apps.AppConfig",
-    "edc_visit_schedule.apps.AppConfig",
-    "edc_timepoint.apps.AppConfig",
-    "edc_registration.apps.AppConfig",
-    "edc_protocol.apps.AppConfig",
-    "edc_identifier.apps.AppConfig",
-    "edc_device.apps.AppConfig",
-    "edc_pdutils.apps.AppConfig",
-]
-
-DEFAULT_SETTINGS = dict(
+DEFAULT_SETTINGS = DefaultTestSettings(
+    calling_file=__file__,
+    template_dirs=[os.path.join(
+        base_dir, app_name, "tests", "templates")],
     BASE_DIR=base_dir,
-    SITE_ID=10,
-    ALLOWED_HOSTS=['localhost'],
-    ROOT_URLCONF=f'{app_name}.urls',
-    STATIC_URL='/static/',
-    INSTALLED_APPS=installed_apps,
-    DATABASES={
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': join(base_dir, 'db.sqlite3'),
-        },
-    },
-    TEMPLATES=[{
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ]
-        },
-    }],
-    MIDDLEWARE=[
-        'django.middleware.security.SecurityMiddleware',
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    ],
-
-    LANGUAGE_CODE='en-us',
-    TIME_ZONE='UTC',
-    USE_I18N=True,
-    USE_L10N=True,
-    USE_TZ=True,
-
     APP_NAME=app_name,
-    EDC_BOOTSTRAP=3,
-    DASHBOARD_URL_NAMES={
-        "subject_dashboard_url": "edc_pdutils:subject_dashboard_url"},
-
-    DEFAULT_FILE_STORAGE='inmemorystorage.InMemoryStorage',
-    MIGRATION_MODULES=DisableMigrations(),
-    PASSWORD_HASHERS=('django.contrib.auth.hashers.MD5PasswordHasher', ),
-)
-
-if os.environ.get("TRAVIS"):
-    DEFAULT_SETTINGS.update(
-        DATABASES={
-            'default': {
-                'ENGINE': 'django.db.backends.mysql',
-                'NAME': 'edc',
-                'USER': 'travis',
-                'PASSWORD': '',
-                'HOST': 'localhost',
-                'PORT': '',
-            },
-        })
+    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "django.contrib.sites",
+        "django_extensions",
+        "django_crypto_fields.apps.AppConfig",
+        "django_revision.apps.AppConfig",
+        "edc_appointment.apps.AppConfig",
+        "edc_metadata.apps.AppConfig",
+        "edc_visit_tracking.apps.AppConfig",
+        "edc_visit_schedule.apps.AppConfig",
+        "edc_timepoint.apps.AppConfig",
+        "edc_registration.apps.AppConfig",
+        "edc_protocol.apps.AppConfig",
+        "edc_identifier.apps.AppConfig",
+        "edc_device.apps.AppConfig",
+        "edc_pdutils.apps.AppConfig",
+    ],
+    add_dashboard_middleware=True,
+).settings
 
 
 def main():
