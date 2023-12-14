e59ccca2d6f32e8cd4934ac9d649f7c930998dc5
erikvw <ew2789@gmail.com>
2023-12-13 23:55:03 -0600

    test setup

--- README.rst
+++ README.rst
@@ -198,8 +198,8 @@ For example:
 .. |pypi| image:: https://img.shields.io/pypi/v/edc-pdutils.svg
     :target: https://pypi.python.org/pypi/edc-pdutils
 
-.. |actions| image:: https://github.com/clinicedc/edc-pdutils/workflows/build/badge.svg?branch=develop
-  :target: https://github.com/clinicedc/edc-pdutils/actions?query=workflow:build
+.. |actions| image:: https://github.com/clinicedc/edc-pdutils/actions/workflows/build.yml/badge.svg
+  :target: https://github.com/clinicedc/edc-pdutils/actions/workflows/build.yml
 
 .. |codecov| image:: https://codecov.io/gh/clinicedc/edc-pdutils/branch/develop/graph/badge.svg
   :target: https://codecov.io/gh/clinicedc/edc-pdutils
--- runtests.py
+++ runtests.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python
 import logging
-import os
 import tempfile
-from os.path import abspath, dirname
+from pathlib import Path
 
 from edc_test_utils import DefaultTestSettings, func_main
 
 app_name = "edc_pdutils"
-base_dir = dirname(abspath(__file__))
+base_dir = Path(__file__).absolute().parent
 
 project_settings = DefaultTestSettings(
     calling_file=__file__,
     selected_database="mysql",
-    template_dirs=[os.path.join(base_dir, app_name, "tests", "templates")],
+    template_dirs=[str(base_dir / app_name / "tests" / "templates")],
     BASE_DIR=base_dir,
     APP_NAME=app_name,
-    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
+    ETC_DIR=str(base_dir / app_name / "tests" / "etc"),
     EDC_EXPORT_EXPORT_FOLDER=tempfile.mkdtemp(),
+    EDC_SITES_REGISTER_DEFAULT=True,
     INSTALLED_APPS=[
         "django.contrib.admin",
         "django.contrib.auth",
