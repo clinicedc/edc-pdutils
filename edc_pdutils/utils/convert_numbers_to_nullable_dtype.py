8d7e686e04eb09c0107cc88619f6f4d61d475b7a
erikvw <ew2789@gmail.com>
2025-05-17 16:36:41 -0500

    funcs to convert numeric dtypes

---
+++ edc_pdutils/utils/convert_numbers_to_nullable_dtype.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def convert_numbers_to_nullable_dtype(df: pd.DataFrame) -> pd.DataFrame:
+    for col in df.select_dtypes(include=["number"]).columns:
+        if pd.api.types.is_float_dtype(df[col]):
+            df[col] = df[col].astype("Float64")
+        elif pd.api.types.is_integer_dtype(df[col]):
+            df[col] = df[col].astype("Int64")
+    return df
